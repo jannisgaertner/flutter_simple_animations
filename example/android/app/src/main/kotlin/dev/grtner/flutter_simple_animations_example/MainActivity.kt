1a3a1647d73b5bdad386c84f1624567a20c55cad
Jannis <87816121+jannisgaertner@users.noreply.github.com>
2022-09-24 23:13:27 +0200

    removed unused plattform specific code

--- example/android/app/src/main/kotlin/dev/grtner/flutter_simple_animations_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.grtner.flutter_simple_animations_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ios/Classes/FlutterSimpleAnimationsPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterSimpleAnimationsPlugin : NSObject<FlutterPlugin>
-@end
